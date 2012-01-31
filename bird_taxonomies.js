c89704d75dbf08250aca9d5cbdcc4bbcc9c00d01
Carlos <carlos@neurotic.es>
2012-01-31 12:32:10 +0100

    Cambios en la maquetación y en el JS

--- bird_taxonomies.js
+++ bird_taxonomies.js
@@ -2,10 +2,37 @@
 
   Drupal.behaviors.bird_taxonomies = {
     attach: function (context, settings) {
-      $('.orders > li > a').click(function() {
-		    $(this).parent('li').children('.item-list').children('ul').children('.families').toggle('slow');
+      $('#block-bird-taxonomies-bird-orders h2').append('<span class="plus">+</span>');
+    	
+    	$('#block-bird-taxonomies-bird-orders .content ul li a').append('<span class="plus">+</span>');
+    	
+      $('.orders > li > a > span.plus').click(function() {
+        plus = $(this);
+		    $(this).parent('a').next('.item-list').children('ul').toggle('slow', function() {
+		      if($(plus).text() == '+') {
+		        $(plus).text('-');
+		        /*alert("Poniendolo a -")*/
+		      }
+		      else {
+		        $(plus).text('+');
+		        /*alert("Poniendolo a +")*/
+		      }
+		    });
 		    return false;
     	});
+    	
+    	$('#block-bird-taxonomies-bird-orders h2').click(function() {
+		    $(this).parent().children('.content').toggle('slow', function() {
+		      if($(this).parent().children('h2').children('span').text() == '+') {
+		        $(this).parent().children('h2').children('span').text('-');
+		      }
+		      else {
+		        $(this).parent().children('h2').children('span').text('+');
+		      }
+		    });
+		    return false;
+    	});
+
     }
   };
 
